2aafc4383a900d586b85c204dcc8dd339caf91d4
AndriiMysko <usandrewmisko@gmail.com>
2021-12-22 16:11:28 +0200

    Add TAM image logs and build status commands

--- lib/travis/cli/tam.rb
+++ lib/travis/cli/tam.rb
@@ -1,21 +1,29 @@
 require 'travis/cli'
+require 'travis/tools/safe_string'
+require 'travis/tools/system'
 
 module Travis
   module CLI
     class Tam < ApiCommand
+      include Tools::SafeString
+
       description "TAM (Travis Artifact Manager) actions"
 
       on('-c', '--create-image IMAGE_NAME', 'Create image with given name')
       on('-u', '--update-image IMAGE_NAME', 'Update image with given name')
       on('-d', '--delete-image IMAGE_NAME', 'Delete image with given name')
       on('-i', '--image-info IMAGE_NAME', 'Get info for image with given name')
+      on('-l', '--image-logs IMAGE_NAME', 'Get logs for image with given name')
+      on('-s', '--image-build-status IMAGE_NAME', 'Get build status for image with given name')
 
       def run
-        error("Please specify an action") if !create_image? && !update_image? && !delete_image? && !image_info?
+        error("Please specify an action") if !create_image? && !update_image? && !delete_image? && !image_info? && !image_logs? && !image_build_status?
         authenticate
 
         image_action if create_image? || update_image? || delete_image?
-        get_image_info if image_info?
+        get_image_info(image_info) if image_info?
+        display_image_log(image_logs) if image_logs?
+        get_image_build_status(image_build_status) if image_build_status?
       end
 
       def image_action
@@ -48,15 +56,43 @@ module Travis
         end
       end
 
-      def get_image_info
-        response = session.get_raw("v3/artifacts/#{image_info}/info")
+      def get_image_info(image_name)
+        response = session.get_raw("v3/artifacts/#{image_name}/info")
 
         if response
           say color('Travis Artifacts image info:', :important)
           say color("Name: #{response['name']}\n", :info)
           say color("Description: #{response['description']}\n", :info)
-          say color("Config content: #{response['config_content']}\n", :info)
+          say color("Config content:\n#{response['config_content']}\n", :info)
           say color("Image size: #{response['image_size']}", :info)
+        end
+      end
+
+      def display_image_log(image_name)
+        info "displaying logs for image #{color(image_name, [:bold, :info])}"
+
+        log = session.get_raw("v3/artifacts/#{image_name}/logs")['log']
+        print_log(log)
+      end
+
+      def print_log(part)
+        print interactive? ? encoded(part) : clean(part)
+      end
+
+      def get_image_build_status(image_name)
+        response = session.get_raw("v3/artifacts/#{image_name}/build_status")
+
+        if response
+          say color('Travis Artifacts image build status:', :important)
+          say color(response['status'], [state_color(response['status']), :bold])
+        end
+      end
+
+      def state_color(state)
+        case state
+        when 'created', 'queued', 'received', 'started' then 'yellow'
+        when 'passed', 'ready'                then 'green'
+        when 'errored', 'canceled', 'failed'  then 'red'
         end
       end
     end
